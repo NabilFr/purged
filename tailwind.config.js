6f76a615e0ca48b702250f6c0240c73fcae7949e
NabilFr <benweb31@gmail.com>
2021-02-14 23:34:29 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    enabled: false,
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
